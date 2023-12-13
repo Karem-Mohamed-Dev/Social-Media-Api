2a3eb905e03dfe91220c0de2b1ea72af6104d752
Karim <karem109k@gmail.com>
2023-12-13 11:03:17 +0200

    Enhance Auth Controllers

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,7 +4,6 @@ const jwt = require("jsonwebtoken");
 const { errorModel } = require("../utils/errorModel")
 const { isEmail } = require("validator");
 const sendEmail = require('../utils/sendEmail');
-const crypto = require("crypto");
 
 const CreateToken = (_id, name) => {
     const token = jwt.sign({ _id, name }, process.env.SECRET, { expiresIn: "3d" });
@@ -19,22 +18,18 @@ exports.login = async (req, res, next) => {
     if (!isEmail(email)) return next(errorModel(400, "Please enter a valid email"));
     if (password.length < 6) return next(errorModel(400, "Password must be more than 6"));
 
-
     try {
-        const user = await User.findOne({ email });
+        const user = await User.findOne({ email }, ["-resetPass", "-posts", "-saved", "-followers", "-followings", "-updatedAt", "-__v"]);
         if (!user) return next(errorModel(404, "User not found"));
 
         const isValidPass = await bcrypt.compare(password, user.password)
-
         if (!isValidPass) return next(errorModel(401, "Email or Password may be wrong"));
 
         const token = CreateToken(user._id, user.name);
-        const { password: pass, updatedAt, __v, ...info } = user._doc
-        res.status(200).json({ ...info, token })
+        const { password: pass, ...info } = user._doc
 
-    } catch (error) {
-        next(error);
-    }
+        res.status(200).json({ ...info, token })
+    } catch (error) { next(error) }
 }
 
 // Register
@@ -47,23 +42,23 @@ exports.register = async (req, res, next) => {
     if (password.length < 6) return next(errorModel(400, "Password must be more than 6"));
 
     try {
-        const exist = await User.findOne({ email });
+        const exist = await User.findOne({ email }, "_id");
         if (exist) return next(errorModel(400, "User already exists"));
 
         const hash = await bcrypt.hash(password, 10);
-        const user = await User.create({ name, email, password: hash });
+        const user = await User.create({ name, email, password: hash },);
 
         const token = CreateToken(user._id, user.name);
-        const { password: pass, updatedAt, __v, ...info } = user._doc
-        res.status(200).json({ ...info, token })
-    } catch (error) {
-        next(error);
-    }
+        const { password: pass, __v, updatedAt, posts, saved, followers, followings, resetPass, ...info } = user._doc
+
+        res.status(200).json({ token, ...info })
+    } catch (error) { next(error) }
 }
 
 // Get Code
 exports.getCode = async (req, res, next) => {
     const { email } = req.body;
+    if(!email) return next(errorModel(400, "Email is required"));
 
     const generateCode = () => {
         let code = "";
@@ -75,9 +70,9 @@ exports.getCode = async (req, res, next) => {
     try {
         const user = await User.findOne({ email });
         if (!user) return next(errorModel(404, "User not found"));
+
         user.resetPass.code = generateCode();
-        const expireDate = Date.now() + (60 * 60 * 1000);
-        user.resetPass.expire = expireDate;
+        user.resetPass.expire = Date.now() + (60 * 60 * 1000);
         await user.save();
 
         await sendEmail(email, "Reset Password", `
@@ -87,24 +82,22 @@ exports.getCode = async (req, res, next) => {
             </div>
         `);
         res.status(200).json({ msg: `Email Sent To: ${email}` });
-    } catch (error) {
-        return next(500, "Something Went Wrong Please Try Again");
-    }
-
+    } catch (error) { next(error) }
 }
 
 // Verify Code
 exports.verifyCode = async (req, res, next) => {
     const { email, code } = req.body;
+    if(!email || !code) return next(errorModel(400, "All fields are required"));
 
     try {
         const user = await User.findOne({ email });
         if (!user) return next(errorModel(404, "User not found"));
 
-        if(user.resetPass.code !== code) return next(errorModel(404, "Wrong Code"));
-        if(user.resetPass.expire < Date.now()) return next(errorModel(404, "Code Expired"));
+        if (user.resetPass.code !== code) return next(errorModel(404, "Wrong Code"));
+        if (user.resetPass.expire < Date.now()) return next(errorModel(404, "Code Expired"));
 
-        const token = jwt.sign({code: user.resetPass.code, _id: user._id}, process.env.SECRET, {expiresIn: '10m'});
+        const token = jwt.sign({ code: user.resetPass.code, _id: user._id }, process.env.SECRET, { expiresIn: '10m' });
         await user.save();
 
         res.status(200).json({ token });
@@ -114,20 +107,21 @@ exports.verifyCode = async (req, res, next) => {
 // Set New Code
 exports.setNewPass = async (req, res, next) => {
     const { token, newPassword } = req.body;
+    if(!token || !newPassword) return next(errorModel(400, "All fields are required"));
 
     try {
         const encoded = jwt.verify(token, process.env.SECRET);
-        console.log(encoded)
+        
         const user = await User.findById(encoded._id);
         if (!user) return next(errorModel(404, "User not found"));
 
-        if(user.resetPass.code !== encoded.code) return next(errorModel(404, "Wrong Code"));
-        if(user.resetPass.expire < Date.now()) return next(errorModel(404, "Code Expired"));
-        
+        if (user.resetPass.code !== encoded.code) return next(errorModel(404, "Wrong Code"));
+        if (user.resetPass.expire < Date.now()) return next(errorModel(404, "Code Expired"));
+
         const hash = await bcrypt.hash(newPassword, 10);
         user.password = hash;
         await user.save();
 
-        res.status(200).json({ msg: "Succes" });
+        res.status(200).json({ msg: "Password Changed" });
     } catch (error) { next(error) }
 }
